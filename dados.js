98106151d0270850d12ea7159eda0525fd4f8e45
Lívia <eailiviass@gmail.com>
2024-09-20 16:38:37 -0300

    fazendo deploy

---
+++ dados.js
@@ -0,0 +1,51 @@
+let dados = [
+  {
+    titulo: "Khun Sam (Sarocha Chankimha - Freen)",
+    descricao:
+      "Khun Sam é uma mulher elegante e poderosa. Ela é rica, inteligente e carismática, sendo admirada e respeitada por todos ao seu redor. Sua postura fria e distante pode esconder seus verdadeiros sentimentos. Khun Sam também enfrenta pressões familiares e sociais, especialmente por ser uma figura pública. Ela é o interesse amoroso de Mon na série.",
+    link: "https://www.instagram.com/srchafreen/",
+    tags: "lady",
+  },
+  {
+    titulo: "Mon (Rebecca Patrícia Armstrong - Becky)",
+    descricao:
+      "Mon é uma jovem dedicada, apaixonada e determinada. Desde pequena, ela idolatra Khun Sam e, ao crescer, se torna sua assistente. Mon é leal, trabalhadora e mantém um carinho muito especial por Khun Sam, que vai além da admiração profissional. Sua trajetória envolve se reconectar com a mulher que ela admira e descobrir os sentimentos que surgem entre elas.",
+    link: "https://www.instagram.com/beccca/",
+    tags: "esposa",
+  },
+  {
+    titulo: "Jim (Orntara Poolsak - Nam)",
+    descricao:
+      "Jim é uma das amigas de Mon, com quem ela compartilha momentos importantes. Ela é uma personagem extrovertida, divertida e muitas vezes oferece alívio cômico na série. Jim também demonstra grande lealdade e apoio a Mon em seus desafios.",
+    link: "https://www.instagram.com/namorntaraaa/",
+    tags: "irma",
+  },
+  {
+    titulo: "Kade (Punnisa Sirisang - Baitoey)",
+    descricao:
+      "Kade é uma personagem vibrante e carismática, conhecida pelo seu comportamento animado. Ela é amiga próxima de Mon e parte do grupo de apoio que a cerca. Sua personalidade alegre contrasta com algumas das situações mais sérias da série, proporcionando momentos leves.",
+    link: "https://www.instagram.com/baitoey_punnisa/",
+    tags: "colega",
+  },
+  {
+    titulo: "Tee (Natnicha Vorrakittikun - Noey)",
+    descricao:
+      "Tee é uma personagem reservada, mas extremamente amigável e carinhosa. Ela é próxima de Mon e faz parte do círculo de amigos que sempre está ao lado dela. Tee é leal e oferece conselhos valiosos a Mon durante os altos e baixos de sua vida.",
+    link: "https://www.instagram.com/noeyvrk/",
+    tags: "parceira",
+  },
+  {
+    titulo: "Yuki (Aurassaya Malaiwong - Irin)",
+    descricao:
+      "Yuki é uma personagem forte e determinada. Ela é uma mulher independente que segue sua própria jornada, mantendo uma personalidade assertiva e direta. Yuki também desempenha um papel importante no grupo de apoio a Mon, sendo uma figura confiante e sensata.",
+    link: "https://www.instagram.com/irinning/",
+    tags: "amiga",
+  },
+  {
+    titulo: "Nop (Ratchanon Kanpiang - Non)",
+    descricao:
+      "Nop é um personagem gentil e atencioso, que também tem uma queda por Mon. Ele é um amigo fiel, sempre disposto a ajudá-la e apoiá-la, embora seus sentimentos não sejam correspondidos da mesma maneira. Nop é uma pessoa boa e altruísta, aceitando seu papel na vida de Mon sem impor seus sentimentos a ela.",
+    link: "https://www.instagram.com/nonn_rk/",
+    tags: "amigo ",
+  },
+];
